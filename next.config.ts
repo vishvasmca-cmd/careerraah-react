52d6ca626c0ca6aa1fee72cd0f1123bae6f15eae
vishvas dhengula <vishvas.dhengula@gmail.com>
2025-12-07 20:27:27 +0000

    try https://media.licdn.com/dms/image/v2/D5612AQEqdyEYYPW4fg/article-cov

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'media.licdn.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
