0e53283ee6c3325750a79af1c9672536aa112df2
vishvas dhengula <vishvas.dhengula@gmail.com>
2025-12-08 00:13:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9907757616-8d615",
+  "appId": "1:234772627014:web:8b24f6843fb7778ab8d447",
+  "apiKey": "AIzaSyANeEIsCmt1C4bOKG4QWFZH_9tOrRafyzM",
+  "authDomain": "studio-9907757616-8d615.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "234772627014"
+};
