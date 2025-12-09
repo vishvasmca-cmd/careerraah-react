4e24674cfc9ca01be337b5ca585ff0cf61567b30
vishvas dhengula <vishvas.dhengula@gmail.com>
2025-12-09 00:17:01 +0000

    try https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7IcJP28kGfwls

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -37,6 +38,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'scilindia.org',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
         port: '',
         pathname: '/**',
       }
